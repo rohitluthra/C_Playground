50e898612482133e7a020269fa4000de4d1a0dce
Rohit Luthra <rohitluthra9@outlook.com>
2021-01-02 01:06:37 -0500

    Structured code in pointer & memory / pointers and dynamic memory allocation.

---
+++ Pointers/4_Pointers_And_Arrays/main.c
@@ -0,0 +1,5 @@
+//
+// Created by Rohit Luthra on 1/2/21.
+//
+
+#include "main.h"
---
+++ Pointers/4_Pointers_And_Arrays/main.h
@@ -0,0 +1,10 @@
+//
+// Created by Rohit Luthra on 1/2/21.
+//
+
+/*
+    #ifndef POINTERS_MAIN_H
+    #define POINTERS_MAIN_H
+    #endif //POINTERS_MAIN_H
+*/
+
