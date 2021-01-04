ddaec27eef8ace7b2c1ee8bfa08c7ca71ed14cbc
Rohit Luthra <rohitluthra9@outlook.com>
2021-01-03 19:17:32 -0500

    summary_of_string_placement added

--- Pointers/5_Pointers_And_Strings/main.c
+++ Pointers/5_Pointers_And_Strings/main.c
@@ -11,6 +11,7 @@
  **********************************************************************************************************************/
 #include "stdio.h"
 #include "stdlib.h"
+#include "string.h"
 
 /**********************************************************************************************************************
  * Function Declaration Starts
@@ -22,12 +23,15 @@ void string_declaration();
 
 void string_initialization();
 
+void summary_of_string_placement();
+
+void standard_string_operation();
+
 /**********************************************************************************************************************
  * Function Declaration Ends
  **********************************************************************************************************************/
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
 
     // ---------------------------------------------------------------------------------------------------------------
     printf("string_fundamentals: \n");
@@ -35,7 +39,7 @@ int main(int argc, char* argv[])
     printf("\n");
 
     // ---------------------------------------------------------------------------------------------------------------
-    printf("string_fundamentals: \n");
+    printf("string_declaration: \n");
     string_declaration();
     printf("\n");
 
@@ -43,14 +47,61 @@ int main(int argc, char* argv[])
     printf("string_initialization: \n");
     string_initialization();
     printf("\n");
+
+    // ---------------------------------------------------------------------------------------------------------------
+    printf("summary_of_string_placement: \n");
+    summary_of_string_placement();
+    printf("\n");
+
+    // ---------------------------------------------------------------------------------------------------------------
+    printf("standard_string_operation: \n");
+    standard_string_operation();
+    printf("\n");
+
+
     return 0;
+}
+
+void standard_string_operation() {
+
+/*
+    Comparing String
+*/
+
+}
+
+void summary_of_string_placement() {
+
+    // IMPORTANT --------> "Header" is stored in String Literal Pool
+
+    // char *globalHeader = "Header";                                       s// Placed in Global Memory but points to string literal pool address
+    // char global_header_array[] = "Global Header";                        // Placed in Global Memory
+
+    // static char *static_header = "Header";                                  // Placed in Global Memory but pointers to header in String Literal Pool
+    // static char static_array_header[] = "Static Char Array";                // Placed in Global Memory
+
+    // char *local_header = "Header";                                          // Placed in Heap but points to string literal pool address
+    // char local_array_header[] = "local array header";                       // Placed in Heap
+    // char *heap_header = (char *) malloc(strlen("heap header" + 1)); // Placed in Heap and address given by malloc in also in heap.So it points to address in heap.
+
+    // strcpy(heap_header, "heap header");
+
 }
 
 void string_initialization() {
     /*
+     * - When a string is initialized, we can use a string literal or a series of characters, or obtain the characters
+     *   from a different source such as standard input.
      *
+     * - Total length is 22 but 23 bytes are required to represent the literal and hold the string.
+     * - The initialization will copy these characters to the array terminated by the NUL character,
      * */
+    char header[] = "This is String Literal";
+    printf("String literal array: %s \n", header);
+    // or
 
+    char header_1[23];
+    strcpy(header_1, "This is String Literal");
 }
 
 void string_declaration() {
@@ -59,29 +110,46 @@ void string_declaration() {
      * String declarations are supported in one of three ways:
      *      1. A literal:
      *              - The string literal is a sequence of characters enclosed in double quotes.
-     *                String literals are frequently used for initialization purposes. They are located in a string literal pool
+     *
+     *              - String literals are frequently used for initialization purposes. This string is stored at read-only
+     *                memory and trying to modify this memory leads to undefined behavior. sThey are located in a string literal pool
+     *
+     *              - When literals are defined they are frequently assigned to a literal pool.
+     *                This area of memory holds the character sequences making up a string.
+     *                When a literal is used more than once, there is normally only a single copy of the string in the string literal pool.
+     *                This will reduce the amount of space needed for the application.
      *
      *      2. As an array of characters:
+     *              - Allocates memory and copies string "string2" into it, thus modifying it is valid.
      *
      *      3. Using a pointer to a character
+     *              - A common way to initialize this string is to use the malloc and strcpy functions to allocate and copy a literal to the string,
      *
      * String literals are not to be confused with characters enclosed in single quotes—these are character literals.
      * */
 
-    // 1. GCC allows following but in many compilers, string literals are treated as constant hence it is immutable.
-    // usually, it is not recommended.
 
     char *tabHeader = "Sound"; // This is string literal
-    *tabHeader = 'L';
-    printf("%s\n",tabHeader); // Displays "Lound"
 
-    const char *tab_header = "Sound";
+    // Bus Error: 10 (also known as SIGBUS and is usually signal 10) occur when a process is trying to access memory that the CPU cannot physically address.
+    //  *tabHeader = 'L'; // Not allowed, we can not modify string literals. This makes tabHeader to point to the read-only memory where your string literal is stored
 
-    //2.
-    char header[32];
+    printf("%s\n", tabHeader);
+
+    const char *tab_header = "Sound"; // Immutable
+    printf("%s\n", tab_header);
+
+    // 2.  Allocates memory and copies string "string2" into it, thus modifying it is valid.
+    char header[32] = "This is not string litral array";
+    header[0] = 'L'; // Ths is allowed
+    printf("header[32] = %s\n", header);
 
     //3.
-    char *header_1;
+    char *header_1 = (char *) malloc(strlen("Media Player") + 1);
+    strcpy(header_1, "Media Player");
+    printf("*header_1 = %s\n", header_1);
+    free(header_1);
+
 
 }
 
