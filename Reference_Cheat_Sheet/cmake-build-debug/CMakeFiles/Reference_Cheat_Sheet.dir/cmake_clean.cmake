ffff6683c7e615967b2ecbab46ac431b8f053049
Rohit Luthra <rohitluthra9@outlook.com>
2021-02-25 19:33:27 -0500

    Make files

---
+++ Reference_Cheat_Sheet/cmake-build-debug/CMakeFiles/Reference_Cheat_Sheet.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Reference_Cheat_Sheet.dir/main.c.o"
+  "Reference_Cheat_Sheet"
+  "Reference_Cheat_Sheet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Reference_Cheat_Sheet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
